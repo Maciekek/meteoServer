a17b7d13ecef88b48cd806da4f64124ac4157f2e
maciekek <maciekek@gmail.com>
2014-12-07 22:53:02 +0100

    fix problem with white character in file with temperature

--- serwer.js
+++ serwer.js
@@ -6,8 +6,15 @@ var httpServer = require("http").createServer(app);
 var Q = require('q');
 var fs = require('fs');
 var names = [];
-var data = {'names':[]};
-var allTemperaturesOne = [[],[],[],[]];
+var data = {
+	'names': []
+};
+var allTemperaturesOne = [
+	[],
+	[],
+	[],
+	[]
+];
 
 var time;
 
@@ -15,23 +22,26 @@ app.use(express.static(__dirname + '/app'));
 
 
 var getNamesConfing = function() {
-	
+
 	var config = fs.readFileSync('./confing.json', "UTF-8");
 
 	config = JSON.parse(config);
 	names = config.names;
-	time = config.time*1000;
-	console.log(time);
+	time = config.time * 1000;
+	console.log("Interwał: " + time);
 }();
 
 httpServer.listen(3000, function() {
 	console.log('Express server listening on port ' + 3000);
-	
+
 });
 
 
 
 var extractTemperature = function(temps) {
+	if(!temps){
+		return;
+	}
 	var temperatures = temps.split("      ");
 	temperatures = temperatures.slice(1, temperatures.length);
 	return temperatures;
@@ -39,7 +49,14 @@ var extractTemperature = function(temps) {
 }
 
 var getLastRecord = function(array) {
-	return array[array.length - 1];
+	
+	if(array[array.length - 1].length==0)
+	{
+		return array[array.length - 2];	
+	}
+	else {
+		return array[array.length - 1];		
+	}
 }
 
 var getTemperature = function() {
@@ -67,27 +84,24 @@ var getNewTemperatureToList = function() {
 	var actualtemperatureRecord = getLastRecord(array);
 
 	actualtemperatureRecord = extractTemperature(actualtemperatureRecord);
-
-	var temp = actualtemperatureRecord[0].match(/\d*/g)[0];
-	console.log(parseInt(temp).constructor);
-	allTemperaturesOne.push(parseInt(temp));
-
+	
 
 	for (var i = 0; i < 4; i++) {
 		data['names'][i] = names[i];
-		var temp = actualtemperatureRecord[i].match(/\d*/g)[0];
+		try {
+			var temp = actualtemperatureRecord[i].match(/\d*/g)[0];
 			allTemperaturesOne[i]
-		allTemperaturesOne[i].push(parseInt(temp));
+			allTemperaturesOne[i].push(parseInt(temp));
+			console.log("OK")
+		}catch(ex){
+			console.log("Program nie może odczytać danych z pliku! Być może plik jest pusty, lub zmieniło się formatowanie tekstu");
+		}
 
 	}
-
-
-
 }
 
 
 setInterval(function() {
-	console.log(time);
 	getNewTemperatureToList()
 }, time);
 
@@ -103,9 +117,9 @@ app.get('/getTemperature', function(req, res) {
 	res.send(data);
 })
 
-app.get('/getTemperatureData', function(req,res){
+app.get('/getTemperatureData', function(req, res) {
 	var temperature = getTemperature();
-	
+
 	res.send(temperature);
 
 })
